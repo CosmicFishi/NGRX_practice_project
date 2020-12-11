8a03c05829505f8797ec164e3c1deaae6fc45ec1
Lý Quốc Dũng <lyquocdung099@gmail.com>
2020-12-11 12:12:58 +0700

    added vietnames to the main page with the use of i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-import { LoginComponent } from './login/login.component';
-import { reducer, ScoreReducer, weatherReducer } from './login.reducer'
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from 'src/environments/environment';
-import { ProfileComponent } from './profile/profile.component';
-import { ScoreComponent } from './score/score.component';
 import { EffectsModule } from '@ngrx/effects';
 import { HttpClientModule } from '@angular/common/http';
-import { InfoEffect, ListEffect, WeatherEffect } from './app.effects';
+import { environment } from 'src/environments/environment';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ScoreComponent } from './score/score.component';
 import { WeatherComponent } from './weather/weather.component';
 import { ListPersonComponent } from './list-person/list-person.component';
-import { infoReducer, ListReducer } from './app.reducer';
 import { ListCardComponent } from './list-card/list-card.component';
 import { GsapExampleComponent } from './gsap-example/gsap-example.component';
 import { CovidPanelComponent } from './covid-panel/covid-panel.component';
 import { ServiceWorkerModule } from '@angular/service-worker'
+
+import { reducer, ScoreReducer, weatherReducer } from './login.reducer'
+import { infoReducer, ListReducer } from './app.reducer';
+import { InfoEffect, ListEffect, WeatherEffect } from './app.effects';
 
 @NgModule({
   declarations: [
